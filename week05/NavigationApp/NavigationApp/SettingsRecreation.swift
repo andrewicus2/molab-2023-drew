3c0f5d7cab6e3883929f2fd1ed48ad9f01c7fd0c
Drew Brown <76884812+andrewicus2@users.noreply.github.com>
2023-10-12 10:11:55 -0400

    Add files via upload

--- week05/NavigationApp/NavigationApp/SettingsRecreation.swift
+++ week05/NavigationApp/NavigationApp/SettingsRecreation.swift
@@ -14,11 +14,89 @@ import SwiftUI
 
 struct SettingsRecreation: View {
     
-    
+    @State private var airplaneMode = false
 
     var body: some View {
         List {
+            Section {
+                NavigationLink(destination: Text("User Name")) {
+                    HStack {
+                        Image("memoji-2")
+                            .resizable()
+                            .frame(width: 65, height: 65)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading) {
+                            Text("Test User")
+                                .font(.system(size: 24))
+                            Text("Apple ID, iCloud+, Media & Purchases")
+                                .font(.caption)
+                        }
+                        .padding(.leading, 10.0)
+                    }
+                }
+                NavigationLink(destination: Text("Family")) {
+                    HStack {
+                        HStack(spacing: -10){
+                            Image("memoji-1")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                                .overlay(
+                                    Circle()
+                                        .stroke(Color.white, lineWidth: 1.5)
+                                )
+                            Image("memoji-4")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .background(Color(red: 0.35,green: 0.35, blue: 0.82))
+                                .clipShape(Circle())
+                                .overlay(
+                                    Circle()
+                                        .stroke(Color.white, lineWidth: 1.5)
+                                )
+                            Image("memoji-3")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                                .overlay(
+                                    Circle()
+                                        .stroke(Color.white, lineWidth: 1.5)
+                                )
+                        }
+                        
+                        Text("Family")
+                    }
+                }
+                NavigationLink(destination: Text("Update Apple ID Settings")) {
+                    HStack{
+                        Text("Update Apple ID Settings")
+                        Spacer()
+                        Text("1")
+                            .frame(width: 26, height: 26)
+                            .background(Color.red)
+                            .clipShape(Circle())
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.trailing, 10.0)
+                }
+            }
             Section{
+                HStack {
+                    ZStack{
+                        Rectangle()
+                            .fill(Color.orange)
+                            .frame(width: 28, height: 28)
+                            .cornerRadius(5)
+                        Image(systemName: "airplane")
+                            .foregroundColor(Color.white)
+                    }
+                    Toggle(isOn: $airplaneMode) {
+                        Text("Airplane Mode")
+                    }
+                }
                 NavigationLink(destination: Text("Wi-Fi")) {
                     HStack {
                         ZStack{
@@ -204,7 +282,7 @@ struct General: View {
             }
         }
         .listStyle(GroupedListStyle())
-        .navigationBarTitle("General")
+        .navigationBarTitle("General", displayMode: .inline)
     }
 }
 
@@ -215,7 +293,7 @@ struct About: View {
     var body: some View {
         Form {
             Section {
-                NavigationLink(destination: EditName(deviceName: deviceName)){
+                NavigationLink(destination: EditName(deviceName: $deviceName)){
                     HStack {
                         Text("Name")
                         Spacer()
@@ -245,7 +323,7 @@ struct About: View {
 
 struct EditName: View {
     
-    @State var deviceName: String
+    @Binding var deviceName: String
 
 
     var body: some View {
