f1bec0e748ec894896e9ca789f624feecdfdef1d
Drew Brown <andrewrylandbrown@gmail.com>
2023-10-18 23:00:21 -0400

    Completed BetterRest App
    Uses CoreML to calculate sleep time

--- week06/BetterRest/BetterRest/ContentView.swift
+++ week06/BetterRest/BetterRest/ContentView.swift
@@ -5,26 +5,93 @@
 //  Created by drew on 10/18/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    @State private var recommendedSleep = 0
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 30
+        return Calendar.current.date(from: components) ?? Date.now
+    }
    
     var body: some View {
-        VStack {
-            Text(Date.now, format: .dateTime.day().month().year())
-            Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationView {
+            Form {
+                Section {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("When do you want to wake up?")
+                            .font(.headline)
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Desired amount of sleep")
+                            .font(.headline)
+                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Daily coffee intake")
+                            .font(.headline)
+                        
+                        Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    }
+                } header: {
+                    Text("Input values")
+                }
+                
+                Section {
+                    VStack(spacing: 5) {
+                        Text("Recommended Bedtime")
+                            .font(.headline)
+                        Text("\(calculateBedtime())")
+                            .font(.largeTitle)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationTitle("Better Rest")
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
-    func exampleDates() {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
-        
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+    func calculateBedtime() -> String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            showingAlert = true
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating!"
+        }
+                
+        return ""
     }
 }
 
