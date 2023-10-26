ffdd39ff32015171c5f6bd167f6b0cdcf8bbcafa
Drew Brown <andrewrylandbrown@gmail.com>
2023-10-26 02:35:28 -0400

    UI Updates

--- week07/SeeFood/SeeFood/ContentView.swift
+++ week07/SeeFood/SeeFood/ContentView.swift
@@ -5,118 +5,168 @@
 //  Created by drew on 10/19/23.
 //
 
-//https://medium.com/@ios_guru/swiftui-and-image-recognition-77d6622a2a7c
+// https://medium.com/@ios_guru/swiftui-and-image-recognition-77d6622a2a7c
 
+
+// https://www.youtube.com/watch?v=6_k0kXhRajo
+
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-let-users-select-pictures-using-photospicker
+
+// https://static.wikia.nocookie.net/silicon-valley/images/d/da/SeeFood.jpg/revision/latest?cb=20180504153534
 
 import CoreML
 import Vision
 import SwiftUI
 import PhotosUI
 
-extension UIImage {
-    
-    // https://www.hackingwithswift.com/whats-new-in-ios-11
-    func toCVPixelBuffer() -> CVPixelBuffer? {
-           
-           let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-             var pixelBuffer : CVPixelBuffer?
-             let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-             guard (status == kCVReturnSuccess) else {
-               return nil
-             }
-
-             CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-             let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-
-             let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-             let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-
-             context?.translateBy(x: 0, y: self.size.height)
-             context?.scaleBy(x: 1.0, y: -1.0)
-
-             UIGraphicsPushContext(context!)
-             self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-             UIGraphicsPopContext()
-             CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-
-             return pixelBuffer
-       }
-}
-
 struct ContentView: View {
     
     var imageClassifier: SeeFoodClassifier?
     
-    @State private var classLabel: String = ""
+    @State private var classLabel: String?
     
-    @State private var avatarItem: PhotosPickerItem?
+    @State private var imageItem: PhotosPickerItem?
     @State private var imageSel: UIImage?
-
+    
     
     init() {
         do {
-        imageClassifier = try SeeFoodClassifier(configuration: MLModelConfiguration())
+            imageClassifier = try SeeFoodClassifier(configuration: MLModelConfiguration())
         } catch {
             print(error)
         }
     }
     var body: some View {
-        VStack {
-            
+        ZStack {
             if let imageSel {
                 Image(uiImage: imageSel)
                     .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 200)
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            } else {
+                Image("HotDog1")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
             }
-            
-            Button("Run") {
-                if let imageSel {
-                    identifyImage(uiImage: imageSel)
+            VStack {
+                if imageSel != nil {
+                    VStack(spacing: -20) {
+                        if(classLabel == "Not Hot Dog") {
+                            Text(classLabel ?? "ERROR")
+                                .fontWeight(.heavy)
+                                .font(.system(size: 60))
+                                .frame(maxWidth: .infinity, maxHeight: 120)
+                                .background(Color.seeFoodRed)
+                                .foregroundStyle(.white)
+                                .shadow(color: .black, radius: 3)
+                            ZStack {
+                                Color(Color.seeFoodRed)
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(.circle)
+                                Image(systemName: "xmark")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .foregroundStyle(.white)
+                            }
+                            
+                        } else {
+                            Text(classLabel ?? "ERROR")
+                                .fontWeight(.heavy)
+                                .font(.system(size: 60))
+                                .frame(maxWidth: .infinity, maxHeight: 120)
+                                .background(Color.seeFoodGreen)
+                                .foregroundStyle(.white)
+                                .shadow(color: .black, radius: 3)
+                            ZStack {
+                                Color(Color.seeFoodGreen)
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(.circle)
+                                Image(systemName: "checkmark")
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                    }
+                } else {
+                    VStack(spacing:0) {
+                        Text("SEEFOOD")
+                            .fontWeight(.heavy)
+                            .font(.system(size: 65))
+                            .frame(maxWidth: .infinity, maxHeight: 90)
+                            .background(Color.seeFoodRed)
+                            .foregroundStyle(.white)
+                            .shadow(color: .black, radius: 3)
+                        
+                        Text("'The Shazam for Food'")
+                            .font(.system(size: 25))
+                            .frame(maxWidth: .infinity, maxHeight: 50)
+                            .background(.white)
+                            .border(.black, width: 3)
+                            .foregroundStyle(Color.seeFoodRed)
+                            .fontWeight(.bold)
+                    }
                 }
-            }
-                .padding()
-            Text("Image is \(classLabel)")
-            .padding()
-        }
-        .padding()
-        
-        VStack {
-            PhotosPicker(selection: $avatarItem, matching: .images) {
-                Text("Select Photo")
-            }
-
-        }
-        .onChange(of: avatarItem) { _ in
-            Task {
-                if let data = try? await avatarItem?.loadTransferable(type: Data.self) {
-                    imageSel = UIImage(data: data)
+                
+                Spacer()
+                
+                VStack {
+                    PhotosPicker(selection: $imageItem, matching: .images) {
+                        
+                        VStack {
+                            Image(systemName: "square.and.arrow.up.circle.fill")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .foregroundStyle(Color.seeFoodRed)
+                                .background(.white)
+                                .clipShape(.circle)
+                            Text("Touch to SEEFOOD")
+                                .frame(maxWidth: .infinity, maxHeight: 60)
+                                .padding()
+                                .font(.system(size: 40))
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                                .shadow(color: .black, radius: 3)
+                        }
+                        
+                    }
                 }
-
-                print("Failed")
+                .onChange(of: imageItem) {
+                    Task {
+                        if let data = try? await imageItem?.loadTransferable(type: Data.self) {
+                            imageSel = UIImage(data: data)
+                        }
+                        
+                        if let imageSel {
+                            identifyImage(uiImage: imageSel)
+                        }
+                        
+                        print("Failed")
+                    }
+                }
             }
         }
     }
     
     func identifyImage(uiImage: UIImage) {
         guard let pixelBuffer = uiImage.toCVPixelBuffer() else { return }
-
+        
         do {
             
             let result = try imageClassifier?.prediction(image: pixelBuffer)
-    
+            
             classLabel = result?.target ?? ""
         } catch {
             print(error)
         }
     }
-    
-    
-    func picker() {
-        
-    }
 }
+
+
+
 
 #Preview {
     ContentView()
 }
+
