73336b9154ca584b5940aa0fb68409b0c0eeb517
Drew Brown <andrewrylandbrown@gmail.com>
2023-11-02 08:25:47 -0400

    Object detection functioning

--- week08/CameraTesting/CameraTesting/Detector.swift
+++ week08/CameraTesting/CameraTesting/Detector.swift
@@ -5,13 +5,15 @@
 //  Created by drew on 11/1/23.
 //
 
-import UIKit
-import AVFoundation
 import Vision
+import AVFoundation
+import UIKit
 
 extension ViewController {
+    
     func setupDetector() {
-        let modelURL = Bundle.main.url(forResource: "YOLOv3TinyInt8LUT", withExtension: "mlmodelc")
+        let modelURL = Bundle.main.url(forResource: "YOLOv3Int8LUT", withExtension: "mlmodelc")
+    
         do {
             let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL!))
             let recognitions = VNCoreMLRequest(model: visionModel, completionHandler: detectionDidComplete)
@@ -29,6 +31,22 @@ extension ViewController {
         })
     }
     
+    func extractDetections(_ results: [VNObservation]) {
+        detectionLayer.sublayers = nil
+        
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else { continue }
+            
+            // Transformations
+            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(screenRect.size.width), Int(screenRect.size.height))
+            let transformedBounds = CGRect(x: objectBounds.minX, y: screenRect.size.height - objectBounds.maxY, width: objectBounds.maxX - objectBounds.minX, height: objectBounds.maxY - objectBounds.minY)
+            
+            let boxLayer = self.drawBoundingBox(transformedBounds)
+
+            detectionLayer.addSublayer(boxLayer)
+        }
+    }
+    
     func setupLayers() {
         detectionLayer = CALayer()
         detectionLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
@@ -39,38 +57,21 @@ extension ViewController {
         detectionLayer?.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
     }
     
-    func extractDetections(_ results: [VNObservation]) {
-        detectionLayer.sublayers = nil // Remove all previous detections
-     
-        for observation in results where observation is VNRecognizedObjectObservation {
-            guard let objectObservation = observation as? VNRecognizedObjectObservation else {  continue }
-     
-            // TODO: Coordinate transformations
-            // ...
-            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(screenRect.size.width), Int(screenRect.size.height))
-            let transformBounds = CGRect(x: objectBounds.minX, y: screenRect.size.height - objectBounds.maxY, width: objectBounds.maxX - objectBounds.minX, height: objectBounds.maxY - objectBounds.minY)
-            
-            let boxLayer = self.drawBoundingBox(transformBounds)
-            
-            detectionLayer.addSublayer(boxLayer)
-        }
-    }
-    
     func drawBoundingBox(_ bounds: CGRect) -> CALayer {
         let boxLayer = CALayer()
         boxLayer.frame = bounds
         boxLayer.borderWidth = 3.0
-        boxLayer.borderColor = CGColor.init(red: 7.0, green: 8.0, blue: 7.0, alpha: 1.0)
+        boxLayer.borderColor = CGColor.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         boxLayer.cornerRadius = 4
         return boxLayer
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
-            
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:]) // Create handler to perform request on the buffer
+
         do {
-            try imageRequestHandler.perform(self.requests)
+            try imageRequestHandler.perform(self.requests) // Schedules vision requests to be performed
         } catch {
             print(error)
         }
--- week08/CameraTesting/CameraTesting/ViewController.swift
+++ week08/CameraTesting/CameraTesting/ViewController.swift
@@ -5,45 +5,83 @@
 //  Created by drew on 11/1/23.
 //
 
-import Foundation
 import UIKit
 import SwiftUI
 import AVFoundation
 import Vision
 
+
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
     private var permissionGranted = false
+    
     private let captureSession = AVCaptureSession()
     private let sessionQueue = DispatchQueue(label: "sessionQueue")
     private var previewLayer = AVCaptureVideoPreviewLayer()
+    
     var screenRect: CGRect! = nil
+    
+    
     
     private var videoOutput = AVCaptureVideoDataOutput()
     
     var requests = [VNRequest]()
     var detectionLayer: CALayer! = nil
     
+      
     override func viewDidLoad() {
         checkPermission()
         
         sessionQueue.async { [unowned self] in
             guard permissionGranted else { return }
             self.setupCaptureSession()
+            
+            self.setupLayers()
+            self.setupDetector()
+            
             self.captureSession.startRunning()
         }
     }
     
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        screenRect = UIScreen.main.bounds
+        self.previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+
+        switch UIDevice.current.orientation {
+            
+            case UIDeviceOrientation.portraitUpsideDown:
+                self.previewLayer.connection?.videoRotationAngle = 0
+             
+            
+            case UIDeviceOrientation.landscapeLeft:
+                self.previewLayer.connection?.videoRotationAngle = 0
+            
+            
+            case UIDeviceOrientation.landscapeRight:
+                self.previewLayer.connection?.videoRotationAngle = 0
+             
+            
+            case UIDeviceOrientation.portrait:
+                self.previewLayer.connection?.videoRotationAngle = 90
+                
+            default:
+                break
+            }
+        
+        updateLayers()
+    }
+    
     func checkPermission() {
-        switch AVCaptureDevice.authorizationStatus(for: .video){
-        case .authorized:
-            permissionGranted = true
-            
-        case .notDetermined:
-            requestPermission()
-            
-        default:
-            permissionGranted = false
-        }
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
+                permissionGranted = true
+                
+            case .notDetermined:
+                requestPermission()
+                    
+            default:
+                permissionGranted = false
+            }
     }
     
     func requestPermission() {
@@ -55,39 +93,39 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     func setupCaptureSession() {
-        // Access camera
+        // Camera input
         guard let videoDevice = AVCaptureDevice.default(.builtInDualWideCamera,for: .video, position: .back) else { return }
         guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
-            
+           
         guard captureSession.canAddInput(videoDeviceInput) else { return }
         captureSession.addInput(videoDeviceInput)
-                          
-        // TODO: Add preview layer
+                         
+        // Preview layer
         screenRect = UIScreen.main.bounds
         
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill // Fill screen
-
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         previewLayer.connection?.videoRotationAngle = 90
         
+        // Detector
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sampleBufferQueue"))
         captureSession.addOutput(videoOutput)
         
         videoOutput.connection(with: .video)?.videoRotationAngle = 90
         
         // Updates to UI must be on main queue
-       DispatchQueue.main.async { [weak self] in
-           self!.view.layer.addSublayer(self!.previewLayer)
-       }
+        DispatchQueue.main.async { [weak self] in
+            self!.view.layer.addSublayer(self!.previewLayer)
+        }
     }
 }
 
 struct HostedViewController: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> some UIViewController {
+    func makeUIViewController(context: Context) -> UIViewController {
         return ViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-    }
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
 }
