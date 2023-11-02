07bc3214236d63ce2175413772b80115492909d4
Drew Brown <andrewrylandbrown@gmail.com>
2023-11-02 09:58:13 -0400

    Labels on ml identifications
    Can't figure out padding!!

--- week08/CameraTesting/CameraTesting/Detector.swift
+++ week08/CameraTesting/CameraTesting/Detector.swift
@@ -5,11 +5,12 @@
 //  Created by drew on 11/1/23.
 //
 
-// https://www.neuralception.com/detection-app-tutorial-detector/
+// Source: https://www.neuralception.com/detection-app-tutorial-detector/
 
 import Vision
 import AVFoundation
 import UIKit
+import CoreImage
 
 extension ViewController {
     
@@ -38,13 +39,14 @@ extension ViewController {
         
         for observation in results where observation is VNRecognizedObjectObservation {
             guard let objectObservation = observation as? VNRecognizedObjectObservation else { continue }
-            
+                        
             let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(screenRect.size.width), Int(screenRect.size.height))
             let transformedBounds = CGRect(x: objectBounds.minX, y: screenRect.size.height - objectBounds.maxY, width: objectBounds.maxX - objectBounds.minX, height: objectBounds.maxY - objectBounds.minY)
             
-            let boxLayer = self.drawBoundingBox(transformedBounds)
+            let boxLayer = self.drawBoundingBox(transformedBounds, label:objectObservation.labels[0].identifier)
 
             detectionLayer.addSublayer(boxLayer)
+            
         }
     }
     
@@ -58,11 +60,28 @@ extension ViewController {
         detectionLayer?.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
     }
     
-    func drawBoundingBox(_ bounds: CGRect) -> CALayer {
+    func drawBoundingBox(_ bounds: CGRect, label: String) -> CALayer {
         let boxLayer = CALayer()
+        
+        // Source: https://stackoverflow.com/questions/2209734/add-text-to-calayer/56159695#56159695
+        
+        let textlayer = CATextLayer()
+
+        textlayer.frame = CGRect(x: 0, y: -18, width: bounds.width, height: 20)
+        textlayer.cornerRadius = 4
+        textlayer.fontSize = 14
+        textlayer.alignmentMode = .left
+        textlayer.string = label
+        textlayer.isWrapped = true
+        textlayer.truncationMode = .end
+        textlayer.backgroundColor = UIColor.red.cgColor
+        textlayer.foregroundColor = UIColor.white.cgColor
+        textlayer.contentsScale = UIScreen.main.scale
+        boxLayer.addSublayer(textlayer)
+        
         boxLayer.frame = bounds
-        boxLayer.borderWidth = 3.0
-        boxLayer.borderColor = CGColor.init(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        boxLayer.borderWidth = 2.0
+        boxLayer.borderColor = UIColor.red.cgColor
         boxLayer.cornerRadius = 4
         return boxLayer
     }
