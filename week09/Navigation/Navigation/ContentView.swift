15180f98596916adbaf781750d9bdf2cb7f54cff
Drew Brown <andrewrylandbrown@gmail.com>
2023-11-07 09:04:37 -0500

    Started Navigation tutorials

---
+++ week09/Navigation/Navigation/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by drew on 11/6/23.
+//
+
+import SwiftUI
+
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            }
+            .navigationDestination(for: Student.self) { student in
+                Text("You selected \(student.name)")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
