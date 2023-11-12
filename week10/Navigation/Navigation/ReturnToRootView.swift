b25d5accee3c69972c0b7c47652a48768badd66d
Drew Brown <andrewrylandbrown@gmail.com>
2023-11-12 13:51:39 -0500

    NavigationStack Paths

--- week10/Navigation/Navigation/ReturnToRootView.swift
+++ week10/Navigation/Navigation/ReturnToRootView.swift
@@ -5,13 +5,73 @@
 //  Created by drew on 11/12/23.
 //
 
-// https://www.hackingwithswift.com/books/ios-swiftui/how-to-make-a-navigationstack-return-to-its-root-view-programmatically
+//  Part 1: https://www.hackingwithswift.com/books/ios-swiftui/how-to-make-a-navigationstack-return-to-its-root-view-programmatically
+
+//  Part 2: https://www.hackingwithswift.com/books/ios-swiftui/how-to-save-navigationstack-paths-using-codable
 
 import SwiftUI
 
-struct ReturnToRootView: View {
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+//    @Binding var path: [Int]
+    @Binding var path: NavigationPath
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+//                    path.removeAll()
+                    path = NavigationPath()
+                }
+            }
+    }
+}
+
+struct ReturnToRootView: View {
+//    @State private var path = [Int]()
+    @State private var pathStore = PathStore()
+    
+    var body: some View {
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $pathStore.path)
+                }
+        }
     }
 }
 
